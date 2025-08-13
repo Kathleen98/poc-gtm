7c03cc37a85309d6a2a093bdd5327a405d9b4182
Kathleen Santos <kathleen.santos@csacademy.com.br>
2025-08-13 16:02:19 -0300

    feat: add pages

---
+++ src/app/Contatos/page.tsx
@@ -0,0 +1,12 @@
+const Contato = () => {
+    return (
+        <div className="">
+            <h1>Page contato</h1>
+            <nav>
+                <a href="/Home">Home</a>
+            </nav>
+        </div>
+    )
+}
+
+export default Contato
---
+++ src/app/Home/page.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return <div className="">
+        <h1>essa é a home</h1>
+        <nav>
+            <a href="/Contatos">Contatos</a>
+        </nav>
+    </div>
+}
+
+export default Home
